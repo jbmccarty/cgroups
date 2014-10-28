196fe40fd7c7aacacc554452d50dfb2ca0723d54
Jason McCarty <jmccarty@sent.com>
2014-10-28 18:33:51 -0400

    Move defaultConfig out of CGroup.hs, and do some renaming.

--- CGroup.hs
+++ CGroup.hs
@@ -1,3 +1,6 @@
+-- This module uses the filesystem to manipulate cgroups. Errors are
+-- reported as strings.
+
 module CGroup where
 import Control.Applicative
 import Control.Arrow
@@ -10,22 +13,19 @@ type PID = Integer
 
 -- configuration data
 data Config = Config {
-  cgroup_root :: FilePath
+  cgroup_root :: FilePath -- root of the cgroup filesystem;
+                          -- shouldn't end in "/"
 }
 
--- change this if necessary
-defaultConfig :: Config
-defaultConfig = Config "/sys/fs/cgroup"
-
--- monad for using configuration data and reporting errors
-type M a = ReaderT Config IO a
+-- monad for using configuration data
+type CM a = ReaderT Config IO a
 
 -- extract the value from the monad, converting any exceptions to
 -- strings
--- Perhaps we should only catch file access exceptions, since anything
--- else is probably a bug.
-runM :: Config -> M a -> IO (Either String a)
-runM c m = left showE <$> try (runReaderT m c) where
+-- Perhaps we should only catch certain exceptions so as not to hide
+-- programming errors.
+runCM :: Config -> CM a -> IO (Either String a)
+runCM c m = left showE <$> try (runReaderT m c) where
   showE :: SomeException -> String
   showE = show
 
@@ -34,27 +34,34 @@ runM c m = left showE <$> try (runReaderT m c) where
 -- not creating other links to directories we shouldn't access.
 -- Alternative: use System.FilePath.Canonical.canonicalFilePath to
 -- check if we left cgroup_root.
-sanitize :: FilePath -> M ()
+sanitize :: FilePath -> CM ()
 sanitize p = when (any (== "..") $ splitDirectories p) $
              fail "\"..\" not allowed in paths"
 
 -- filesystem path of a cgroup
-path :: FilePath -> M FilePath
+path :: FilePath -> CM FilePath
 path cg = do
   sanitize cg
   (++ ("/" ++ cg)) . cgroup_root <$> ask
 
 -- filesystem path of the tasks file for a cgroup
-tasks :: FilePath -> M FilePath
+tasks :: FilePath -> CM FilePath
 tasks cg = (++ "/tasks") <$> path cg
 
 -- create a cgroup
-createCG :: FilePath -> M ()
-createCG cg = path cg >>= liftIO . createDirectory
+createCGroup :: FilePath -> CM ()
+createCGroup cg = path cg >>= liftIO . createDirectory
 
 -- place a process into a cgroup
-movePID :: PID -> FilePath -> M ()
+movePID :: PID -> FilePath -> CM ()
 movePID pid cg = tasks cg >>= liftIO . flip writeFile (show pid)
 
-listPIDs :: String -> M [Integer]
+listPIDs :: String -> CM [Integer]
 listPIDs cg = tasks cg >>= liftIO . fmap (map read . lines) . readFile
+
+{- Not implemented, but it wouldn't be too difficult:
+ - creating/deleting a hierarchy attached to given controllers
+ - deleting cgroups
+ - listing cgroups/hierarchies/controllers
+ - listing/setting cgroup attributes
+ -}
