9c3835ea3218b76ac33dfa4b0a79b1575bb57796
Jason McCarty <jmccarty@sent.com>
2014-10-29 17:29:29 -0400

    Cleanup and error handling.

--- Main.hs
+++ Main.hs
@@ -1,16 +1,21 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
 import CGroup
+import Control.DeepSeq
+import Control.Exception (IOException)
+import Control.Monad.Catch (handle)
 import Control.Monad.Except
 import Control.Monad.Reader
 import Data.List (intercalate)
-import Network.FastCGI
+import GHC.IO.Exception (ioe_type, IOErrorType(..))
 import Network.CGI.Monad
-import Control.DeepSeq
+import Network.CGI.Protocol
+import Network.FastCGI
 
--- an error consists of a return code and some messages
-type Error = (Int, String, [String])
+-- the CGI library is missing some useful instances...
+instance NFData CGIResult where
+  rnf CGINothing = ()
+  rnf (CGIOutput s) = rnf s
 
--- missing instances for monad transformers
 instance MonadCGI m => MonadCGI (ExceptT e m) where
   cgiAddHeader n s = lift $ cgiAddHeader n s
   cgiGet = lift . cgiGet
@@ -19,18 +24,22 @@ instance MonadCGI m => MonadCGI (ReaderT r m) where
   cgiAddHeader n s = lift $ cgiAddHeader n s
   cgiGet = lift . cgiGet
 
+-- an error consists of a return code and some messages
+type Error = (Int, String, [String])
+
 -- root of the cgroup filesystem; change if necessary
 config :: Config
 config = Config "/sys/fs/cgroup"
 
--- produce a 400 Invalid Request response
-invalid :: MonadError Error m => String -> m a
-invalid s = throwError (400, "Invalid Request", [s])
+-- 400 invalid request error
+invalid :: String -> Error
+invalid s = (400, "Invalid Request", [s])
 
 -- get the value of a parameter, returning a 400 error if it's not present
 getValue :: (MonadError Error m, MonadCGI m) => String -> m String
 getValue s = getInput s >>=
-             maybe (invalid $ "Missing parameter: " ++ s) return
+             maybe (throwError . invalid $ "Missing parameter: " ++ s)
+                   return
 
 -- interpret the value of a parameter, returning a 400 error if it's not
 -- valid
@@ -40,7 +49,7 @@ readValue s = do
   let v = reads s'
   case v of
     [(v', "")] -> return v'
-    _ -> invalid $ "Invalid value for " ++ s ++ ": " ++ s'
+    _ -> throwError . invalid $ "Invalid value for " ++ s ++ ": " ++ s'
 
 {- process a single request, and output the response.
 
@@ -59,38 +68,43 @@ Valid parameters and values:
   name of the cgroup to list. Only HEAD and GET are allowed. A
   space-separated list of pids is returned in the response.
 -}
-process_request :: CGI CGIResult
+processRequest :: CGI CGIResult
 -- here we use ExceptT for early-out behavior, and ReaderT to pass the
 -- cgroup root location.
-process_request = handle_output . runExceptT . flip runReaderT config $ do
-  -- the non-exhaustive pattern matching here is intentional: any
-  -- invalid command syntax results in a pattern match failure, which
-  -- gets interpreted as a 400 invalid request
+processRequest = handleOutput . flip runReaderT config $ do
   cmd <- getValue "command"
   case cmd of
-    "createcgroup" -> do
-      cg <- getValue "cgroup"
-      res <- createCGroup cg
-      deepseq res $ outputNothing
-      -- check res for errors
+    "createcgroup" -> getValue "cgroup" >>= createCGroup >> outputNothing
     "movepid"  -> do
       pid <- readValue "pid"
       cg <- getValue "cgroup"
-      res <- movePID pid cg
-      deepseq res $ outputNothing
-    "listpids" -> do
-      cg <- getValue "cgroup"
-      res <- listPIDs cg
-      -- check res for errors
-      let res' = (++ "\n") . intercalate " " . map show $ res
-      deepseq res' $ output res'
-    _ -> invalid $ "Invalid command: " ++ cmd
+      movePID pid cg
+      outputNothing
+    "listpids" -> getValue "cgroup" >>= listPIDs >>=
+                  output . (++ "\n") . intercalate " " . map show
+    _ -> throwError . invalid $ "Invalid command: " ++ cmd
 
--- check output for error codes (remaining exceptions are caught by
--- handleErrors below and result in a 500 Internal Server Error)
-handle_output :: CGI (Either Error CGIResult) -> CGI CGIResult
-handle_output r = r >>= either h return where
-  h (c, e, es) = outputError c e es
+-- check output for error codes and client-caused exceptions (remaining
+-- exceptions are caught by handleErrors below and result in a 500
+-- Internal Server Error)
+handleOutput :: ExceptT Error (CGIT IO) CGIResult -> CGI CGIResult
+handleOutput r = handleExceptions (runExceptT r) >>=
+                 either handleEither return
+  where
+    -- convert exceptions to Left values to prevent them escaping in a
+    -- thunk (even if they were already Left values)
+    handleExceptions r = h1 handleIO . h1 handlePath $ do
+      res <- r
+      deepseq res $ return res -- make sure any exception is forced
+    h1 f = handle (return . Left . f) -- handle one exception
+    handleIO (e :: IOException) = case ioe_type e of
+      NoSuchThing -> (404, "Not Found", [show e])
+      _ -> (403, "Forbidden", [show e])
+    handlePath (PathException p) = invalid $
+      "\"..\" not allowed as a path component: " ++ p
+
+    -- finally convert any errors to output
+    handleEither (c, e, es) = outputError c e es
 
 main :: IO ()
-main = runFastCGIorCGI $ handleErrors process_request
+main = runFastCGIorCGI $ handleErrors processRequest
