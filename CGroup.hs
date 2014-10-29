90b8beaf2b3e32a484c761529513a2fd38a11ee0
Jason McCarty <jmccarty@sent.com>
2014-10-29 01:14:08 -0400

    Use a context instead of a specialized monad for cgroups.

--- CGroup.hs
+++ CGroup.hs
@@ -1,16 +1,15 @@
--- This module uses the filesystem to manipulate cgroups. Errors are
--- reported as strings.
+-- This module uses the filesystem to manipulate cgroups.
 
-module CGroup where
-import Control.Applicative ((<$>))
-import Control.Arrow (left)
-import Control.DeepSeq (NFData, deepseq)
-import Control.Exception (try, SomeException)
-import Control.Monad (when)
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Reader (ReaderT, runReaderT, ask)
+{-# LANGUAGE FlexibleContexts #-}
+module CGroup (PID, Config(..), createCGroup, movePID, listPIDs) where
+import Control.Monad.Reader
 import System.Directory (createDirectory)
 import System.FilePath (splitDirectories)
+
+-- not necessary once Applicative becomes a superclass of Monad
+(<$>) :: Monad m => (a -> b) -> m a -> m b
+(<$>) = liftM
+infixl 4 <$>
 
 -- process ID
 type PID = Integer
@@ -21,53 +20,38 @@ data Config = Config {
                           -- shouldn't end in "/"
 }
 
--- monad for using configuration data
-type CM a = ReaderT Config IO a
-
--- extract the value from the monad, converting any exceptions to
--- strings
-runCM :: NFData a => Config -> CM a -> IO (Either String a)
-runCM c m = left show <$> tryE r where
-  r = do
-    res <- runReaderT m c
-    deepseq res $ return res
-    -- laziness makes preventing exceptions from escaping the monad
-    -- tricky: we need to force everything that gets returned
-  tryE :: IO a -> IO (Either SomeException a) -- for type inference
-  tryE = try
-
 -- throw an error if a path contains any ".." components, to prevent
 -- accessing files we shouldn't. The administrator is responsible for
 -- not creating other links to directories we shouldn't access.
 -- Alternative: use System.FilePath.Canonical.canonicalFilePath to
 -- check if we left cgroup_root.
-sanitize :: FilePath -> CM ()
+sanitize :: MonadReader Config m => FilePath -> m ()
 sanitize p = when (any (== "..") $ splitDirectories p) $
              fail "\"..\" not allowed in paths"
 
 -- filesystem path of a cgroup
-path :: FilePath -> CM FilePath
+path :: MonadReader Config m => FilePath -> m FilePath
 path cg = do
   sanitize cg
   (++ ("/" ++ cg)) . cgroup_root <$> ask
 
 -- filesystem path of the tasks file for a cgroup
-tasks :: FilePath -> CM FilePath
+tasks :: MonadReader Config m => FilePath -> m FilePath
 tasks cg = (++ "/tasks") <$> path cg
 
 -- create a cgroup
 -- Sanity check: is the parent directory a cgroup?
-createCGroup :: FilePath -> CM ()
+createCGroup :: (MonadReader Config m, MonadIO m) => FilePath -> m ()
 createCGroup cg = path cg >>= liftIO . createDirectory
 
 -- place a process into a cgroup
 -- Sanity check: is cg actually a cgroup?
-movePID :: PID -> FilePath -> CM ()
+movePID :: (MonadReader Config m, MonadIO m) => PID -> FilePath -> m ()
 movePID pid cg = tasks cg >>= liftIO . flip writeFile (show pid ++ "\n")
 
 -- list the pids in a cgroup
 -- Sanity check: is cg actually a cgroup?
-listPIDs :: String -> CM [Integer]
+listPIDs :: (MonadReader Config m, MonadIO m) => FilePath -> m [Integer]
 listPIDs cg = tasks cg >>= liftIO . fmap (map read . lines) . readFile
 
 {- Not implemented, but it wouldn't be too difficult:
